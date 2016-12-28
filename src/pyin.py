dc94b0919b2ccafb44cb5a5b3c27f9a471666717
LemonAtSu <at_7788546@hotmail.com>
2016-12-28 16:22:04 +0800

    add parameters

--- src/pyin.py
+++ src/pyin.py
@@ -2,11 +2,20 @@ import numpy as np
 import vamp
 import librosa
 
-def pYIN(audio, fs=44100., hop_size=1024):
+def pYIN(audio, fs=44100., hop_size=1024, 
+            lowampsuppression=.1, onsetsensitivity=.7, prunethresh=.09):
 
     length = len(audio)
     audio = np.asarray(audio)
-    data = vamp.collect(audio, fs, 'pyin:pyin', 'notes')['list']
+
+    parameters = {
+             'prunethresh' : prunethresh,
+        'lowampsuppression' : lowampsuppression,
+        'onsetsensitivity' : onsetsensitivity,
+    }
+
+    data = vamp.collect(audio, fs, 'pyin:pyin', 'notes', 
+                        parameters=parameters, block_size=2048, step_size=1024)['list']
     actl  = proc_frame(data, length, fs=fs, hop_size=hop_size)
 
     return actl
@@ -21,7 +30,7 @@ def proc_frame(data, length, fs=44100., hop_size=1024, offset=34-1):
     for d in data:
         dur = int(float(d['duration']) * fs)
         st = int(float(d['timestamp']) * fs)
-        midi = int(round(librosa.hz_to_midi(float(d['values'])) - offset))
+        midi = int(np.round(librosa.hz_to_midi(float(d['values'])) - offset))
         samples[st : st+dur] = midi
         hz_samples[st : st+dur] = float(d['values'])
 
