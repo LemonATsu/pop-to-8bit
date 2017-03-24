3d1f87ad94207d28213dbbb393b2c4a0eb1f9d59
AT <at_7788546@hotmail.com>
2017-03-24 11:12:21 +0800

    add a flag to enable the use of SVS background

--- 8bits.py
+++ 8bits.py
@@ -10,6 +10,7 @@ if __name__ == '__main__':
     parser.add_argument("output_path", type=str, help='path of output file')
     parser.add_argument("-s", "--sample_rate", type=float, 
                             help='sample rate of input file', default=44100.)
+    parser.add_argument("-c", help='Use SVS background instead of non-SVS one', action="store_false")
     parser.add_argument("--block_size", type=int, help='block size of pYIN', default=2048)
     parser.add_argument("--step_size", type=int, help='step size of pYIN', default=256)
 
@@ -17,7 +18,7 @@ if __name__ == '__main__':
     print('block_size : %d, step_size : %d' %(args.block_size, args.step_size))
 
     audio, fs = librosa.load(args.audio_path, args.sample_rate, mono=False)
-    audio_8bit = core.convert(audio, fs=fs, block_size=args.block_size, step_size=args.step_size)
+    audio_8bit = core.convert(audio, fs=fs, v_centered=args.c, block_size=args.block_size, step_size=args.step_size)
     librosa.output.write_wav(args.output_path, audio_8bit, sr=int(fs), norm=False)
 
 
